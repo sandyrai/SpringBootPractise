928b9da3f71b8005a29df2f6e3ad7a71f52e6c89
sandyrai <kmr.sandeep1997@gmail.com>
2021-06-26 20:50:27 +0530

    first commit

---
+++ src/main/java/com/spring/practise/SpringBootPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.spring.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPractiseApplication.class, args);
+	}
+
+}
